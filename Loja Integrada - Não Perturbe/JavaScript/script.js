55966e7187e97dbc7b83eb808e40019bb24207ff
rafael-branco <72389630+rafael-branco@users.noreply.github.com>
2021-10-01 15:56:13 -0300

    Add files via upload

--- Loja Integrada - Não Perturbe/JavaScript/script.js
+++ Loja Integrada - Não Perturbe/JavaScript/script.js
@@ -1,17 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){
-
-    document.querySelector("#nao-perturbe").addEventListener('change', function(){
-        if (this.checked) {
-            var currentURL = window.location.href;
-            var substring = "app.lojaintegrada.com.br/painel";
-            if(currentURL.includes(substring)){
-                document.querySelector("body>div#intercom-container").remove();
-            }
-        } else {
-            console.log("Checkbox is not checked..");
-        }
-        
-    });
-
+    console.log("TESTE 1235");
 
 });
